2d15724fa928e30497f626cc39bc70900a4fd52a
Jo'rabek Egamov <134671078+egamovj@users.noreply.github.com>
2024-01-31 01:08:20 +0500

    setup javascript

---
+++ main.js
@@ -0,0 +1,4 @@
+let nextDom = document.getElementById('next');
+let previouseDom = document.getElementById('prev');
+let carouselDom = document.querySelector('.carousel');
+let listItem = document.querySelector('.list');
